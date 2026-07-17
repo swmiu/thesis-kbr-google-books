9d48d7363ab5e45b2f07c7b02d4e8a7b1a68e3e5
swmiu <mushaboom13@gmail.com>
2026-07-17 15:37:14 +0800

    fix: correct page jey

--- website/js/navigation.js
+++ website/js/navigation.js
@@ -91,7 +91,7 @@ function loadPage(pageName) {
             </div>
         `,
         
-        'year_distribution': `
+        'temporal_framework': `
             <h1 style="position: absolute; left: -9999px;">KBR Google Books Analysis - Temporal Patterns</h1>
             <div class="page-banner">
                 <picture>
